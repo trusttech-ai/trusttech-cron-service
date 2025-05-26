a6400756c8f5c57d9d3aa25361535552365ab11a
Ryan Basque <ryanbasquedev@gmail.com>
2025-05-25 22:57:10 -0300

    feat: adjust cron log

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const prisma = new PrismaClient();
 
 const rewindMessagesJob = new RewindMessagesJob(prisma);
 
-cron.schedule('0 9 * * 1-5', () => rewindMessagesJob.execute());
+cron.schedule('0 9 * * 1-5', () => {
+    rewindMessagesJob.execute()
+    console.log('Rewind Messages Job executed at 9 AM on weekdays.');
+});
 
 console.log('Service Initalized.');
