a05eeb78d14208288bdb9af17fb58d65e2422867
Ryan Basque <ryanbasquedev@gmail.com>
2025-05-28 00:42:00 -0300

    feat: dockerfiles

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,21 @@ const prisma = new PrismaClient();
 
 const rewindMessagesJob = new RewindMessagesJob(prisma);
 
+async function testDatabaseConnection() {
+  try {
+    await prisma.$connect();
+    console.log('[Prisma] Database connection established successfully.');
+  } catch (error) {
+    console.error('[Prisma] Failed to connect to the database:', error);
+    process.exit(1);
+  }
+}
+
+testDatabaseConnection();
+
 cron.schedule('0 9 * * 1-5', () => {
     rewindMessagesJob.execute()
-    console.log('Rewind Messages Job executed at 9 AM on weekdays.');
+    console.log('[Rewind] Messages Job executed at 9 AM on weekdays.');
 });
 
-console.log('Service Initalized.');
+console.log('CRON Service Initalized.');
